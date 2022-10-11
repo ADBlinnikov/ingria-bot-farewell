a9284db36486413bbbbec91c330ec698ebc8b257
Andrew Blinnikov <blnnikoff@gmail.com>
2022-10-11 22:31:32 +0300

    Обновил подготовку отчетов с пмоощью s3 sync

--- reporting/data.py
+++ reporting/data.py
@@ -1,6 +1,6 @@
 class User:
     def __init__(self, id=None, first_name=None, last_name=None, username=None, last_modified=None, **kwargs):
-        self.id = id
+        self.id = str(id)
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
@@ -8,10 +8,24 @@ class User:
         if kwargs:
             print(f"Unknown parameters for User: {kwargs.keys()}")
 
+    def __iter__(self):
+        return iter([self.id, self.first_name, self.last_name, self.username, self.last_modified])
+
+    @classmethod
+    def header(self):
+        return ["id", "first_name", "last_name", "username", "last_modified"]
+
 
 class UserFeedback:
-    def __init__(self, user_id, message_id, text, last_modified):
-        self.user_id = user_id
+    def __init__(self, user, message_id, text, last_modified):
+        self.user = user
         self.message_id = message_id
         self.text = text
         self.last_modified = last_modified
+
+    def __iter__(self):
+        return iter([self.user, self.message_id, self.text, self.last_modified])
+
+    @classmethod
+    def header(self):
+        return ["user", "message_id", "text", "last_modified"]
--- reporting/main.py
+++ reporting/main.py
@@ -1,73 +1,72 @@
-import boto3
 import json
-from jinja2 import Environment, select_autoescape, PackageLoader
+import os
 from datetime import datetime
-
+from glob import glob
+from jinja2 import Environment, PackageLoader, select_autoescape
+import csv
 from .data import User, UserFeedback
+from typing import List
+
+os.makedirs("./reports", exist_ok=True)
 
 BUCKET_NAME = "ingria-bot-farewell"
 NOW = datetime.now()
 
 env = Environment(loader=PackageLoader("reporting", "templates"), autoescape=select_autoescape())
 
-session = boto3.session.Session()
-s3 = session.client(service_name="s3", endpoint_url="https://storage.yandexcloud.net")
-
-
-def read(path):
-    get_object_response = s3.get_object(Bucket=BUCKET_NAME, Key=path)
-    return get_object_response["Body"].read(), get_object_response["LastModified"]
-
-
-def iterate(folder):
-    for key in s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=folder)["Contents"]:
-        yield key["Key"]
-
-
-def write(file, path):
-    s3.put_object(Bucket=BUCKET_NAME, Key=path, Body=file, StorageClass="COLD")
-    print(f"File write at path: {path}")
-
-
 # Пройти по списку пользователей и создать отчет
 def generate_index():
     template = env.get_template("index.html")
     page = template.render(title="Выберите отчет")
-    write(page, "index.html")
+    with open("./reports/index.html", "w") as f:
+        f.write(page)
 
 
-def generate_report(type: str):
+def generate_report(type: str) -> List[User]:
     print(f"Start report generation for {type}")
     template = env.get_template("users.html")
     title = f"Пользователи {'начавшие' if type == 'started' else 'завершившие'} квест"
     data = []
-    for file in iterate(f"users/{type}/"):
-        content, modified_at = read(file)
-        user = User(last_modified=modified_at, **json.loads(content))
+    for file in glob(f"./users/{type}/*.json"):
+        modified_at = datetime.fromtimestamp(os.path.getmtime(file))
+        with open(file, "r") as f:
+            user = User(last_modified=modified_at, **json.load(f))
         data.append(user)
     data.sort(key=lambda x: x.last_modified, reverse=True)
-    page = template.render(title=title, data=data, last_update=NOW)
-    write(page, f"reports/{type}.html")
+    with open(f"./reports/{type}.csv", "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(User.header())
+        writer.writerows(data)
+    page = template.render(type=type, data=data, last_update=NOW)
+    with open(f"./reports/{type}.html", "w") as f:
+        f.write(page)
+    return data
 
 
-def generate_feedback_report():
+def generate_feedback_report(users: dict):
     print(f"Start report generation for feedback")
     template = env.get_template("feedback.html")
     data = []
-    for file in iterate("users/feedback/"):
+    for file in glob("./users/feedback/*/*.json"):
         parts = file.split("/")
-        uid = parts[2]
-        mid = parts[3].split(".")[0]
-        text, upd = read(file)
-        feedback = UserFeedback(user_id=uid, message_id=mid, text=text.decode("utf8"), last_modified=upd)
+        uid = parts[-2]
+        mid = parts[-1].split(".")[0]
+        upd = datetime.fromtimestamp(os.path.getmtime(file))
+        with open(file, "r") as f:
+            text = f.read().strip('"')
+        user = users[uid]
+        user_str = f"{user.first_name or ''} {user.last_name or ''} (@{user.username or 'empty'})"
+        feedback = UserFeedback(user=user_str, message_id=mid, text=text, last_modified=upd)
         data.append(feedback)
     data.sort(key=lambda x: x.last_modified, reverse=True)
-    page = template.render(title="Отзывы", data=data, last_update=NOW)
-    write(page, f"reports/feedback.html")
+    page = template.render(data=data, last_update=NOW)
+    with open(f"./reports/feedback.html", "w") as f:
+        f.write(page)
 
 
 if __name__ == "__main__":
     generate_index()
-    generate_feedback_report()
+    users = generate_report("started")
     generate_report("finished")
-    generate_report("started")
+    users = {u.id: u for u in users}
+    generate_feedback_report(users)
