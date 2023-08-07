4517c52fb4f8507e1700fb6edf3ca2bc13695d68
adb <blnnikoff@gmail.com>
2023-08-07 21:27:08 +0300

    minor update

--- src/context.ts
+++ src/context.ts
@@ -4,9 +4,10 @@ export interface MyContext extends Context {
   session: {
     skip_counter: number;
     stage: number;
+    startedAt: string;
   };
 }
 
 export function initialState() {
-  return { skip_counter: 0, stage: 0 };
+  return { skip_counter: 0, stage: 0, startedAt: new Date().toJSON() };
 }
